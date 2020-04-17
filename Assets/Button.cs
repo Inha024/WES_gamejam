8950d75f0109849fc299293b08236fd764f794b6
Lukas <Tsuki32@hotmail.com>
2020-04-17 13:47:34 +0200

    Death and button

---
+++ Assets/Button.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    float curDist = 5;
+    GameObject[] Spike;
+    GameObject Spikes;
+
+    // Use this for initialization
+    void Start()
+    {
+        Spike = GameObject.FindGameObjectsWithTag("Spike");
+        curDist = 5;
+
+        foreach (GameObject item in Spike)
+        {
+            float dist = Vector3.Distance(transform.position, item.transform.position);
+            if (dist > curDist)
+            {
+                curDist = dist;
+                Spikes = item;
+            }
+        }
+    }
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+           if (collision.gameObject.tag == "Player")
+           {
+
+            Destroy(Spikes);
+           }
+        }
+
+}
+
---
+++ Assets/Death.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    
+    public GameObject Button;
+    public GameObject Myplace;
+    // Start is called before the first frame update
+ 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (var myPlace in GameObject.FindGameObjectsWithTag("Myplace"))
+            {
+             Instantiate(Button, myPlace.transform.position, transform.rotation);
+
+            }
+        }
+
+    }
+}
