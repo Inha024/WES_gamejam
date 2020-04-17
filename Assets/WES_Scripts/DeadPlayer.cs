2553686860df052af787f0d58c8357ebb6c3cfc5
Egil <vumsy@hotmail.com>
2020-04-17 13:48:54 +0200

    Deadplayer

--- Assets/WES_Scripts/DeadPlayer.cs
+++ Assets/WES_Scripts/DeadPlayer.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class DeadPlayer : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
     public float xInput;
     public float yInput;
     public float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.isKinematic = true;
     }
 
     // Update is called once per frame
@@ -28,7 +30,7 @@ public class DeadPlayer : MonoBehaviour
     {
         if (xInput != 0 || yInput != 0)
         {
-            transform.position += new Vector3(xInput * speed * Time.deltaTime, yInput * speed * Time.deltaTime, transform.position.z);
+            transform.position += new Vector3(xInput * speed * Time.deltaTime, yInput * speed * Time.deltaTime, transform.position.z * 0);
         }
     }
 }
