c1d2e980190e49ae71c56afdd8a7149d9c4adc85
Egil <vumsy@hotmail.com>
2020-04-17 13:28:52 +0200

    Klar for animasjon av gå og hopping
    Laget spilleren som prefab

---
+++ Assets/WES_Scripts/DeadPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadPlayer : MonoBehaviour
+{
+    public float xInput;
+    public float yInput;
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        ApplyMovement();
+    }
+    void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+    }
+    void ApplyMovement()
+    {
+        if (xInput != 0 || yInput != 0)
+        {
+            transform.position += new Vector3(xInput * speed * Time.deltaTime, yInput * speed * Time.deltaTime, transform.position.z);
+        }
+    }
+}
